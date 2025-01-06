9eaad11f8838cca8140de1d185500ff6246e0ac5
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-06 22:22:07 +0100

    styled components created for Loading

---
+++ src/features/Portfolio/Projects/Loading/styled.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { ReactComponent as SpinnerSign } from "../../../../images/spinner.svg";
+
+export const Wrapper = styled.div`
+  max-width: 475px;
+  margin: auto;
+  text-align: center;
+`;
+
+export const Message = styled.p`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 28px;
+  color:${({ theme }) => theme.text.header};
+  margin: 88px 0 48px ;
+`;
+
+export const SpinnerIcon = styled(SpinnerSign)`
+  color:${({ theme }) => theme.spinner};
+  width: 160px;
+  height: 160px;
+  animation: 1s linear normal infinite rotation;
+
+  @keyframes rotation {
+    from{
+      transform: rotate(0deg);
+    }
+    to{
+      transform: rotate(360deg);
+    }
+  }
+`;
