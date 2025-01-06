31644562edebc30380bc9b1da04b9cd19dfc8641
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-06 22:28:23 +0100

    store corrections

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
-import { store } from "./store";
+import App from "./main/App";
+import store from "./main/store";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
