55bdb842f14e7d2e369cbce147fb5bcd0c713f34
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-06 22:15:42 +0100

    GlobalStyle created

---
+++ src/main/GlobalStyle.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+box-sizing: border-box;
+}
+
+*, ::before, ::after {
+box-sizing: inherit;
+}
+
+body {
+    background-color: ${({ theme }) => theme.background.main};
+    transition: background 0.5s;
+    font-family: "Inter", sans-serif;
+    margin: 14px;
+    letter-spacing: 0.06em;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.tabletMax}) {
+      margin: 18px;
+    }
+  }
+`;
