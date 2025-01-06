cf9fdb13372fc154ae30ed6b73709f04494f0b7f
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-06 22:25:53 +0100

    Header and styled components created

---
+++ src/features/Portfolio/Header/index.js
@@ -0,0 +1,9 @@
+import { Wrapper, Title, GithubIcon, Caption } from "./styled";
+
+export const Header = () => (
+    <Wrapper>
+        <GithubIcon />
+        <Title>Portfolio</Title>
+        <Caption>My recent projects</Caption>
+    </Wrapper>
+);
---
+++ src/features/Portfolio/Header/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { ReactComponent as GitHub } from '../../../images/github.svg';
+
+export const Wrapper = styled.header`
+  text-align: center;
+`;
+
+export const Title = styled.h2`
+  font-size: 30px;
+  font-weight: 900;
+  line-height: 36.31px;
+  color: ${({ theme }) => theme.text.header};
+  margin-top: 12px;
+  margin-bottom: 8px;
+`;
+
+export const Caption = styled.p`
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 28px;
+  color: ${({ theme }) => theme.text.header};
+  margin: 0;
+`;
+
+export const GithubIcon = styled(GitHub)`
+  color: ${({ theme }) => theme.github};
+  width: 40px;
+  height: 40px;
+`;
