70cca410ae3d3c46c7c71b66fc0abeb6d2d0bb87
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-06 22:26:21 +0100

    Portfolio and styled components created

---
+++ src/features/Portfolio/index.js
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Header } from "./Header";
+import { Projects } from "./Projects";
+import { fetchProjects, selectProjectsState } from "./Projects/portfolioSlice"
+import { useEffect } from "react";
+import { Wrapper } from "./styled";
+import { Loading } from "./Projects/Loading";
+import { Error } from "./Projects/Error";
+
+export const Portfolio = () => {
+    const dispatch = useDispatch();
+    const { loading, error, projects } = useSelector(selectProjectsState);
+
+    useEffect(() => {
+        dispatch(fetchProjects());
+    }, [dispatch]);
+
+    return (
+        <>
+            <Header></Header>
+            {loading ? <Loading /> :
+                error ? <Error /> :
+                    <Wrapper>
+                        {projects.map(project => (<Projects
+                            key={project.id}
+                            title={project.name}
+                            description={project.description}
+                            demoLink={project.homepage}
+                            codeLink={project.html_url}
+                        />))}
+                    </Wrapper>
+            }
+        </>
+    )
+};
