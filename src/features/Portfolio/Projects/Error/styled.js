13892dccfa100af698486893799a319c933a2d0e
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-06 22:22:42 +0100

    styled components created for Error

---
+++ src/features/Portfolio/Projects/Error/styled.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { ReactComponent as AlertSign } from "../../../../images/alert.svg";
+
+export const Wrapper = styled.div`
+  max-width: 426.53px;
+  margin: auto;
+  text-align: center;
+  margin-top: 88px;
+`;
+
+export const Header = styled.h3`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 29.05px;
+  color: ${({ theme }) => theme.text.header};
+  margin: 16px 0 32px;
+`;
+
+export const Message = styled.p`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 28px;
+  color: ${({ theme }) => theme.text.header};
+  margin: 0;
+`;
+
+export const AlertIcon = styled(AlertSign)`
+  width: 37px;
+  height: 33.34px;
+  color: ${({ theme }) => theme.text.header};
+`;
+
+export const Button = styled.a`
+  display: inline-block;
+  margin: 32px 0 140px;
+  background-color: ${({ theme }) => theme.button.background};
+  padding: 12px 16px;
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.button.border};
+  text-decoration: none;
+  color: ${({ theme }) => theme.button.text};
+  font-weight: 600;
+  font-size: 20.06px;
+  line-height: 24.28px;
+  transition: 0.5s;
+
+  &:hover{
+    box-shadow: ${({ theme }) => theme.button.hoverShadow};
+  }
+
+  &:active{
+    box-shadow: ${({ theme }) => theme.button.activeShadow};
+  }
+`;
