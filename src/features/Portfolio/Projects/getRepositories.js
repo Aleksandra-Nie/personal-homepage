dccd6701d176faed54a0304f0feebfbf7f4b629d
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-06 22:25:04 +0100

    getRepositories created

---
+++ src/features/Portfolio/Projects/getRepositories.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { API_URL } from "./apiUrl";
+
+
+export const getRepositories = async () => {
+    try {
+        const response = await axios.get(API_URL);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw new Error(error.message);
+    }
+};
