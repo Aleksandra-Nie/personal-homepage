f72b1ee8352ddda3f41aa5f112d94e0695b25514
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-06 22:24:05 +0100

    portfolioSaga created

---
+++ src/features/Portfolio/Projects/portfolioSaga.js
@@ -0,0 +1,17 @@
+import { call, delay, put, takeLatest } from "redux-saga/effects";
+import { getRepositories } from "./getRepositories";
+import { fetchProjects, fetchProjectsError, fetchProjectsSuccess } from "./portfolioSlice";
+
+function* fetchProjectsHandler() {
+    try {
+        yield delay(2000);
+        const project = yield call(getRepositories);
+        yield put(fetchProjectsSuccess(project));
+    } catch (error) {
+        yield put(fetchProjectsError(error));
+    }
+}
+
+export function* portfolioSaga() {
+    yield takeLatest(fetchProjects.type, fetchProjectsHandler)
+}
