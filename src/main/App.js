f80127b142e9f03da5f57bff7bf1b91f1d3c773b
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-06 22:13:10 +0100

    App created

---
+++ src/main/App.js
@@ -0,0 +1,36 @@
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
+import { useSelector } from "react-redux";
+import { selectDarkMode } from "../features/toggleTheme/themeSlice";
+import { ToggleTheme } from "../features/toggleTheme"
+import { Container } from "../features/Container";
+import { About } from "../features/About";
+import { Section } from "../features/Section";
+import { skills, toLearn } from "../data";
+import { Portfolio } from "../features/Portfolio";
+
+function App() {
+  const darkMode = useSelector(selectDarkMode);
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <Container>
+        <ToggleTheme />
+        <About />
+        <Section
+          title="My skillset includes 🛠️"
+          skills={skills}
+        />
+        <Section
+          title="What I want to learn next 🚀"
+          skills={toLearn}
+        />
+        <Portfolio />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
