18ead3899a500c6c6790b1ec1ecc9f1ae210cd18
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-07 21:41:09 +0100

    socialMedia created

---
+++ src/features/Footer/socialMedia.js
@@ -0,0 +1,15 @@
+import { ReactComponent as GitHubIcon } from "../../images/github.svg";
+import { ReactComponent as LinkedInIcon } from "../../images/linkedin.svg";
+
+export const socialMedia = [
+    {
+        name: "GitHub",
+        url: "https://github.com/Aleksandra-Nie",
+        Icon: GitHubIcon,
+    },
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/aleksandra-niedzia%C5%82kowska-07ab8325a",
+        Icon: LinkedInIcon,
+    },
+];
