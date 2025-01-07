7ad86173b467661be709457c4c0081f7e79f622b
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-07 21:39:56 +0100

    Footer created and styled components added

---
+++ src/features/Footer/index.js
@@ -0,0 +1,41 @@
+import { socialMedia } from "./socialMedia";
+import {
+    Wrapper,
+    Caption,
+    Contact,
+    Description,
+    SocialMedia,
+    IconLink,
+    Icon,
+} from "./styled";
+
+export const Footer = () => (
+    <Wrapper>
+        <Caption>LET'S TALK!</Caption>
+        <Contact href="mailto:aniedzialkowska2@gmail.com">
+            aniedzialkowska2@gmail.com
+        </Contact>
+        <Description>
+            I’m always open to new projects and would love to collaborate!
+            If you're working on a website, dashboard, or mobile app and need help bringing your ideas to life, feel free to reach out.
+            Whether it’s a responsive design or a fully functional app, I’m here to make your vision a reality.
+            Whenever I have the time, I’m ready to take on exciting new challenges and help you turn your ideas into something great.
+            Let’s create something amazing together! 🙌
+        </Description>
+        <SocialMedia>
+            {socialMedia.map(({ name, url, Icon }) => (
+                <li key={name}>
+                    <IconLink
+                        as="a"
+                        href={url}
+                        title={name}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <Icon />
+                    </IconLink>
+                </li>
+            ))}
+        </SocialMedia>
+    </Wrapper>
+)
---
+++ src/features/Footer/styled.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.footer`
+  max-width: 691px;
+  margin-top: 120px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    margin-top: 48px;
+  }
+`;
+
+export const Caption = styled.h2`
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 15.6px;
+  color: ${({ theme }) => theme.text.paragraph};
+  margin: 0 0 24px;
+  letter-spacing: initial;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    margin: 0 0 12px;
+  }
+`;
+
+export const Contact = styled.a`
+  font-weight: 900;
+  font-size: 32px;
+  line-height: 38.73px;
+  color: ${({ theme }) => theme.text.header};
+  text-decoration: none;
+  transition: 0.5s;
+
+  &:hover{
+      color: ${({ theme }) => theme.linkHover};
+    }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    font-size: 18px;
+    line-height: 21.78px;
+  }
+`;
+
+export const Description = styled.p`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 25.2px;
+  color: ${({ theme }) => theme.text.header};
+  margin: 24px 0 56px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    font-size: 14px;
+    line-height: 16.94px;
+    margin:12px 0 40px;
+  }
+`;
+
+export const SocialMedia = styled.ul`
+  display: flex;
+  gap: 24px;
+  list-style: none;
+  padding: 0;
+`;
+
+export const IconLink = styled.div`
+  width: 48px;
+  height: 48px;
+  display: inline-block;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background-color 0.3s;
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+
+  path {
+    fill: ${({ theme }) => theme.text.header};
+
+    &:hover {
+      fill: ${({ theme }) => theme.icon};
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    width: 32px;
+    height: 32px;
+  }
+`;
